96e7c729c46dc67a29fad1377168103bad5a553d
Lucas Peres <lucasperesss@gmail.com>
2022-06-02 11:40:54 -0300

    Back: refatorando models pra função que retorna o model

--- back-end/src/database/models/product.js
+++ back-end/src/database/models/product.js
@@ -1,40 +1,43 @@
-// const { DataTypes, Model } = require('sequelize');
-// const db = require('.');
+const { DataTypes, Model } = require('sequelize');
+const db = require('.');
 
-// class Product extends Model {
-//   id;
-//   name;
-//   price;
-//   urlImage;
-// }
+module.exports = () => {
+  class Product extends Model {
+    id;
+    name;
+    price;
+    urlImage;
+  }
 
-// Product.init(
-//   {
-//     id: {
-//       allowNull: false,
-//       autoIncrement: true,
-//       primaryKey: true,
-//       type: DataTypes.INTEGER,
-//     },
-//     name: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     price: {
-//       type: DataTypes.DECIMAL(4, 2),
-//       allowNull: false,
-//     },
-//     urlImage: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//   },
-//   {
-//     underscored: true,
-//     sequelize: db,
-//     modelName: 'products',
-//     timestamps: false,
-//   }
-// );
+  Product.init(
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      price: {
+        type: DataTypes.DECIMAL(4, 2),
+        allowNull: false,
+      },
+      urlImage: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      underscored: true,
+      sequelize: db,
+      modelName: 'Product',
+      tableName: 'products',
+      timestamps: false,
+    }
+  );
 
-// module.exports = Product;
+  return Product;
+};
--- back-end/src/database/models/salesProducts.js
+++ back-end/src/database/models/salesProducts.js
@@ -1,43 +1,45 @@
-// import { DataTypes, Model } from 'sequelize';
-// import db from '.';
-// import Sale from './sale';
-// import Product from './product';
+const { DataTypes, Model } = require('sequelize');
+const db = require('.');
+const Sale = require('./sale');
+const Product = require('./product');
 
-// class SalesProducts extends Model {
-//   saleId;
-//   productId;
-//   quantity;
-// }
+module.exports = () => {
+  class SalesProducts extends Model {
+    saleId;
+    productId;
+    quantity;
+  }
 
-// SalesProducts.init(
-//   {
-//     saleId: {
-//       allowNull: false,
-//       primaryKey: true,
-//       type: DataTypes.INTEGER,
-//     },
-//     productId: {
-//       allowNull: false,
-//       primaryKey: true,
-//       type: DataTypes.INTEGER,
-//     },
-//     quantity: {
-//       allowNull: false,
-//       type: DataTypes.INTEGER,
-//     },
-//   },
-//   {
-//     underscored: true,
-//     sequelize: db,
-//     modelName: 'salesProducts',
-//     timestamps: false,
-//   }
-// );
+  SalesProducts.init(
+    {
+      saleId: {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      productId: {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      quantity: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+      },
+    },
+    {
+      underscored: true,
+      sequelize: db,
+      modelName: 'salesProducts',
+      timestamps: false,
+    }
+  );
 
-// SalesProducts.belongsTo(Sale, { foreignKey: 'saleId' });
-// SalesProducts.belongsTo(Product, { foreignKey: 'productId' });
+  SalesProducts.belongsTo(Sale, { foreignKey: 'saleId' });
+  SalesProducts.belongsTo(Product, { foreignKey: 'productId' });
 
-// Sale.hasMany(SalesProducts, { foreignKey: 'saleId' });
-// Product.hasMany(SalesProducts, { foreignKey: 'productId' });
+  Sale.hasMany(SalesProducts, { foreignKey: 'saleId' });
+  Product.hasMany(SalesProducts, { foreignKey: 'productId' });
 
-// export default SalesProducts;
+  return SalesProducts;
+};
