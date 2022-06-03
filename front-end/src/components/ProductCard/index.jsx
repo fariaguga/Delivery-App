7f3fa865be8c95f567a89c4caea105cce6f6e294
Wilian Pereira <wilian_pereira92@outlook.com>
2022-06-02 23:40:08 -0300

    front: criando estilo no ProductCar

--- front-end/src/components/ProductCard/index.jsx
+++ front-end/src/components/ProductCard/index.jsx
@@ -1,50 +1,56 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import styles from './styles.module.scss';
 
 function ProductCard({ product }) {
   return (
-    <section>
-      <span
-        data-testid={ `customer_products__element-card-title-${product.id}` }
-      >
-        { product.name }
-      </span>
-
-      <span
-        data-testid={ `customer_products__element-card-price-${product.id}` }
-      >
-        { product.price.toString().replace('.', ',')}
-      </span>
-
-      <img
-        data-testid={ `customer_products__img-card-bg-image-${product.id}` }
-        src={ product.urlImage }
-        alt={ product.name }
-        width="100px"
-        height="100px"
-      />
-
+    <section className={ styles.container }>
       <div>
-        <button
-          type="button"
-          data-testid={ `customer_products__button-card-rm-item-${product.id}` }
+        <span
+          data-testid={ `customer_products__element-card-price-${product.id}` }
         >
-          -
-        </button>
+          R$
+          {' '}
+          { product.price.toString().replace('.', ',')}
+        </span>
 
-        <input
-          type="text"
-          value="0"
-          data-testid={ `customer_products__input-card-quantity-${product.id}` }
+        <img
+          data-testid={ `customer_products__img-card-bg-image-${product.id}` }
+          src={ product.urlImage }
+          alt={ product.name }
+          width="250px"
+          height="250px"
         />
-
-        <button
-          type="button"
-          data-testid={ `customer_products__button-card-add-item-${product.id}` }
-        >
-          +
-        </button>
       </div>
+      <section className={ styles.quantity }>
+        <span
+          data-testid={ `customer_products__element-card-title-${product.id}` }
+        >
+          { product.name }
+        </span>
+
+        <div>
+          <button
+            type="button"
+            data-testid={ `customer_products__button-card-rm-item-${product.id}` }
+          >
+            -
+          </button>
+
+          <input
+            type="text"
+            value="0"
+            data-testid={ `customer_products__input-card-quantity-${product.id}` }
+          />
+
+          <button
+            type="button"
+            data-testid={ `customer_products__button-card-add-item-${product.id}` }
+          >
+            +
+          </button>
+        </div>
+      </section>
     </section>
   );
 }
