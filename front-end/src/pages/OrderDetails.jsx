c23c54f09aff818e94f903748527da7727335ad9
Lucas Peres <lucasperesss@gmail.com>
2022-06-08 21:54:10 -0300

    Front: add NavbarSeller to OrderDetails page

--- front-end/src/pages/OrderDetails.jsx
+++ front-end/src/pages/OrderDetails.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import CardDetails from '../components/CardDetails';
+import NavBarSeller from '../components/NavBarSeller';
 
 function OrderDetails() {
   return (
-    <CardDetails />
+    <>
+      <NavBarSeller />
+      <CardDetails />
+    </>
   );
 }
 
