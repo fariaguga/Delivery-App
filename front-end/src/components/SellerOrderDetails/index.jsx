0dfeb190dcb94b5de6671ec0c69e863db610691f
Wilian Pereira <wilian_pereira92@outlook.com>
2022-06-09 19:34:33 -0300

    front: implementa page seller order details

---
+++ front-end/src/components/SellerOrderDetails/index.jsx
@@ -0,0 +1,141 @@
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import api from '../../services/api';
+import { getLocalStorage } from '../../utils/localStorage';
+
+function SellerOrderDetails() {
+  const location = useLocation();
+  const [order, setOrder] = useState({});
+
+  useEffect(() => {
+    const id = location.pathname.split('seller/orders/')[1];
+    const { token } = getLocalStorage('user');
+
+    const config = {
+      headers: {
+        authorization: token,
+      },
+    };
+    api.get(`/seller/order/${id}`, config)
+      .then((res) => {
+        const sale = res.data;
+        setOrder(sale);
+      });
+  }, [location.pathname]);
+
+  const validateDate = (date) => {
+    const MIN = 10;
+    const formatDate = new Date(date);
+    const day = formatDate.getDate();
+    const month = formatDate.getMonth() + 1;
+    const year = formatDate.getFullYear();
+    const d = `${day < MIN ? `0${day}` : day}`;
+    const m = `${month < MIN ? `0${month}` : month}`;
+    return `${d}/${m}/${year}`;
+  };
+
+  return (
+    <>
+      <section>
+        <h2
+          data-testid="seller_order_details__element-order-details-label-order-id"
+        >
+          {Object.keys(order).length > 0 && order.id}
+        </h2>
+        <h2
+          data-testid="seller_order_details__element-order-details-label-order-date"
+        >
+          {Object.keys(order).length > 0 && validateDate(order.saleDate)}
+        </h2>
+        <h2
+          data-testid="seller_order_details__element-order-details-label-delivery-status"
+        >
+          {Object.keys(order).length > 0 && order.status}
+        </h2>
+        <button
+          type="button"
+          data-testid="seller_order_details__button-preparing-check"
+        >
+          PREPARAR PEDIDO
+        </button>
+        <button
+          type="button"
+          disabled={ Object.keys(order).length > 0 && order.status === 'Pendente' }
+          data-testid="seller_order_details__button-dispatch-check"
+        >
+          SAIU PARA ENTREGA
+        </button>
+      </section>
+      <table>
+        <caption>Finalizar Pedido</caption>
+        <thead>
+          <tr>
+            <th>Item</th>
+            <th>Descrição</th>
+            <th>Quantidade</th>
+            <th>Valor Unitário</th>
+            <th>Sub-total</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.keys(order).length > 0
+          && order.sale.map(({ product, quantity }, index) => (
+            <tr key={ product.id }>
+              <td
+                data-testid={
+                  `seller_order_details__element-order-table-item-number-${index}`
+                }
+              >
+                {index + 1}
+              </td>
+
+              <td
+                data-testid={
+                  `seller_order_details__element-order-table-name-${index}`
+                }
+              >
+                {product.name}
+              </td>
+
+              <td
+                data-testid={
+                  `seller_order_details__element-order-table-quantity-${index}`
+                }
+              >
+                {quantity}
+              </td>
+
+              <td
+                data-testid={
+                  `seller_order_details__element-order-table-unit-price-${index}`
+                }
+              >
+                {(product.price).replace('.', ',')}
+              </td>
+
+              <td
+                data-testid={
+                  `seller_order_details__element-order-table-sub-total-${index}`
+                }
+
+              >
+                {(product.price * quantity)
+                  .toFixed(2).toString().replace('.', ',')}
+
+              </td>
+
+            </tr>
+          ))}
+
+        </tbody>
+      </table>
+      <span
+        data-testid="seller_order_details__element-order-total-price"
+      >
+        {Object.keys(order).length > 0 && (order.totalPrice).replace('.', ',')}
+      </span>
+    </>
+  );
+}
+
+export default SellerOrderDetails;
