53a1cc727fe0c7c8a0065c84320196c5834b599f
Lucas Peres <lucasperesss@gmail.com>
2022-06-08 21:51:19 -0300

    Front: fix lint errors

--- front-end/src/pages/Seller-Orders.jsx
+++ front-end/src/pages/Seller-Orders.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from 'react';
 import CardSales from '../components/CardSales';
 import NavBarSeller from '../components/NavBarSeller';
 
 function SellerOrders() {
   return (
     <div>
-      < NavBarSeller />
-      < CardSales />
-      </div>
-  )
+      <NavBarSeller />
+      <CardSales />
+    </div>
+  );
 }
 
 export default SellerOrders;
